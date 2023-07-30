a481f0081540aaae347d12fbc6ae0097887bd55c
ramtec511 <ramtec51@gmail.com>
2023-07-30 13:14:32 -0600

    rama nueva creada

--- scripts/scrip_prueba.js
+++ scripts/scrip_prueba.js
@@ -1,7 +1,30 @@
+//variables
+
+var form_cuerpo={
+    correo:"",
+    contrasena:"",
+    checar:false
+}
+
 hola_mundo=()=>{
     console.log("boton oprimidos")
 }
 
 la_funcion=()=>{
     console.log("boton la_funcion")
+}
+
+ejecuta_form=(evt)=>{
+    evt.preventDefault();
+    console.log(evt);
+    console.log("valor de las propiedades",form_cuerpo)
+}
+
+captura_onchanche=(evt)=>{
+    console.log("ejecutando evento de captura",evt)
+    if (evt.target.name == 'checar') {
+        form_cuerpo[`${evt.target.name}`]=evt.target.checked
+    }else{
+        form_cuerpo[`${evt.target.name}`]=evt.target.value
+    }
 }
