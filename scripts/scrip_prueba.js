bcf32689f06f5bcae60e3e780759b87048c91f9a
ramtec511 <ramtec@gmail.com>
2023-08-06 13:16:53 -0600

    conexion API

--- scripts/scrip_prueba.js
+++ scripts/scrip_prueba.js
@@ -3,7 +3,8 @@
 var form_cuerpo={
     correo:"",
     contrasena:"",
-    checar:false
+    checar:false,
+    datos_tabla:[]
 }
 
 hola_mundo=()=>{
@@ -14,10 +15,11 @@ la_funcion=()=>{
     console.log("boton la_funcion")
 }
 
-ejecuta_form=(evt)=>{
+ejecuta_form=async(evt)=>{
     evt.preventDefault();
     console.log(evt);
-    console.log("valor de las propiedades",form_cuerpo)
+    console.log("valor de las propiedades",form_cuerpo);
+    validacion_login(form_cuerpo)
 }
 
 captura_onchanche=(evt)=>{
@@ -26,5 +28,57 @@ captura_onchanche=(evt)=>{
         form_cuerpo[`${evt.target.name}`]=evt.target.checked
     }else{
         form_cuerpo[`${evt.target.name}`]=evt.target.value
+    }
+}
+
+validacion_login=async(dat_entrada)=>{
+    try {
+        if (dat_entrada.correo =="ejemplo12@gmail.com" && dat_entrada.contrasena=="12345*") {
+            window.alert("se logueo con exito");
+            obt_all_personajes()
+        }else{
+            window.alert("credenciales incorrectas")
+        }
+        
+    } catch (err) {
+        window.alert('Hubo un error al intentar loguearse')
+    }
+}
+
+mostrar_tabla=()=>{
+    var el_div=document.getElementById("collapseExample");
+    el_div.className="visible"
+}
+
+obt_all_personajes=async()=>{
+    try {
+
+        var respuesta=await axios.get('https://rickandmortyapi.com/api/character').catch(err=>{
+            throw err
+        })
+        var data_new=respuesta.data;
+        if (data_new.results) {
+            form_cuerpo.datos_tabla=data_new.results;
+        }else{
+            throw "no existen datos"
+        }
+
+        var tabla_inner=document.getElementById("cuerpo_tabla");
+
+        form_cuerpo.datos_tabla.forEach(item=>{
+            tabla_inner.innerHTML += ` <tr>
+            <th scope="row"></th>
+            <td>${item.id}</td>        
+            <td>${item.gender}</td>        
+            <td>${item.name}</td> 
+            </tr> `
+        })
+        
+        console.log("respuesta de API",respuesta.data)
+        mostrar_tabla()
+
+    } catch (err) {
+        console.log(err);
+        window.alert("no se pudo conectar a la API")
     }
 }
